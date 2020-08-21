b70df1b8f179cffac95ff88c5d88ba91fddf39e9
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2020-08-22 00:18:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# meetup-to-mdx
+
+## Usage
+
+```
+npm install
+```
+
+```
+npm start <your_file.json>
+```
+
+See output in `talks` folder 👍
---
+++ meetup-to-mdx.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const slugify = require('slugify')
+
+const outputTalksDir = 'talks'
+
+if (!fs.existsSync(outputTalksDir)) {
+  fs.mkdirSync(outputTalksDir)
+}
+
+if (process.argv.length <= 2) {
+  console.log('Missing argument: use node meetup-to-mdx.js <path-to-talks.json>')
+  return
+}
+
+const rawTalks = fs.readFileSync(process.argv[2])
+const talks = JSON.parse(rawTalks)
+
+const getTalkContent = (talk) => {
+  return `---
+edition: ${talk.edition}
+meetupLink: '${talk.meetupLink}'
+title: '${talk.title} #${talk.edition}'
+description:
+speakers:
+---
+
+${talk.content}`
+}
+
+for (const talk of talks) {
+  const title = slugify(talk.title, {
+    lower: true,
+    strict: true
+  })
+
+  fs.writeFile(`${outputTalksDir}/${talk.date}_${title}.mdx`, getTalkContent(talk), (err) => {
+    if (err) return console.log(err)
+  })
+}
